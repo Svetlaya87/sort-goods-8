a46b16e5e73daa9d9ecc2ea2a4c902f5efefef30
Svetlaya87 <svetlaya87@gmail.com>
2021-08-02 23:24:18 +0300

    add flag to sort

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,36 +10,85 @@
         //мой код
         console.log(products);
         
-       
+       let PriceUp=false;
         // сортировка по возрастанию
+        let SortPriceDown = document.getElementById('sortPriceDown');
+        let SortPriceUp = document.getElementById('sortPriceUp');
+
+
 
         
-        let SortPriceUp = document.getElementById('sortPriceUp');
-    
+        if (!PriceUp){
+            SortPriceDown.removeEventListener('click', function(){
+                let divRow = document.querySelector('div.row');
+                divRow.innerHTML ='';
+            });
 
-
-        SortPriceUp.addEventListener( 
-            'click',   function(products){
-                products.sort( 
+                products.sort(
                     function(a, b) {
                         return a.price - b.price;
                     }
-                )
-            } );
+                );
 
-        console.log(products);
         
-        // сортировка по убыванию
+                    
+            
+            SortPriceUp.addEventListener('click', function(){
+                let divRow = document.querySelector('div.row');
+                divRow.innerHTML = products.map(item => `
+        
+                    <div class="cell p-2">
+                        <div class="card p-2 pt-4 h-100 shadow">
+                            <img src="${item.image}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                            <h5 class="card-title">${item.title}</h5>
+                            <p class="card-text">${item.description.substring(0, 100)}...</p>
+                            <h4 class='text-end'>$${item.price}</h4>
+                            </div>
+                        </div>
+                    </div>
+                `).join('');
+            });  
 
-        let SortPriceDown = document.getElementById('sortPriceDown');
-        SortPriceDown.addEventListener( 
-            'click', function(products){
-                products.sort( 
-                    function(a, b) {
-                        return -(a.price - b.price);
-                    }
-                )
-            } );
+            console.log(products);
+        } else {     
+        
+
+            
+          
+            // сортировка по убыванию
+            PriceUp=false;
+            SortPriceUp.removeEventListener('click', function(){
+                let divRow = document.querySelector('div.row');
+                divRow.innerHTML ='';
+            });
+            
+
+            products.sort( 
+                function(a, b) {
+                    return -(a.price - b.price);
+                }
+            );
+
+            SortPriceDown.addEventListener( 
+                        'click', function(){
+                            let divRow = document.querySelector('div.row');
+                            divRow.innerHTML = products.map(item => `
+                    
+                        <div class="cell p-2">
+                            <div class="card p-2 pt-4 h-100 shadow">
+                                <img src="${item.image}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                <h5 class="card-title">${item.title}</h5>
+                                <p class="card-text">${item.description.substring(0, 100)}...</p>
+                                <h4 class='text-end'>$${item.price}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    `).join('');
+                        }); 
+        }       
+
         console.log(products);
           
 
@@ -47,9 +96,9 @@
 
         //конец моего кода
         
-    let divRow = document.querySelector('div.row');
+    
 
-    divRow.innerHTML = products.map(item => `
+    /*divRow.innerHTML = products.map(item => `
     
         <div class="cell p-2">
             <div class="card p-2 pt-4 h-100 shadow">
@@ -61,7 +110,7 @@
                 </div>
             </div>
         </div>
-    `).join('');
+    `).join('');*/
 
 
 })();
