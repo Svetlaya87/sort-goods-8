d16a35d8061d106088452b89e4f33e2a5d8e7f74
Svetlaya87 <svetlaya87@gmail.com>
2021-08-03 01:13:49 +0300

    add funct drawCards

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,111 +6,67 @@
     let products = await fetch(URL);
 
         products = await products.json();
+        let divRow = document.querySelector('div.row');
 
-        //мой код
-        console.log(products);
+        function drawCards() {
+            divRow.innerHTML = products.map(item => `
         
-       let PriceUp=false;
-        // сортировка по возрастанию
+            <div class="cell p-2">
+                <div class="card p-2 pt-4 h-100 shadow">
+                    <img src="${item.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-text">${item.description.substring(0, 100)}...</p>
+                    <h4 class='text-end'>$${item.price}</h4>
+                    </div>
+                </div>
+            </div>`).join('');
+        };
         let SortPriceDown = document.getElementById('sortPriceDown');
         let SortPriceUp = document.getElementById('sortPriceUp');
-
-
+        
+        // вывожу сразу картинки с сортировкой по возврастанию
+        products.sort(
+            function(a, b) {
+                 return a.price - b.price;
+            }
+        );
+        drawCards();
+       
 
         
-        if (!PriceUp){
-            SortPriceDown.removeEventListener('click', function(){
-                let divRow = document.querySelector('div.row');
-                divRow.innerHTML ='';
-            });
-
-                products.sort(
-                    function(a, b) {
-                        return a.price - b.price;
-                    }
-                );
-
+        console.log(products);
         
-                    
-            
-            SortPriceUp.addEventListener('click', function(){
-                let divRow = document.querySelector('div.row');
-                divRow.innerHTML = products.map(item => `
-        
-                    <div class="cell p-2">
-                        <div class="card p-2 pt-4 h-100 shadow">
-                            <img src="${item.image}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                            <h5 class="card-title">${item.title}</h5>
-                            <p class="card-text">${item.description.substring(0, 100)}...</p>
-                            <h4 class='text-end'>$${item.price}</h4>
-                            </div>
-                        </div>
-                    </div>
-                `).join('');
-            });  
-
-            console.log(products);
-        } else {     
-        
-
-            
-          
-            // сортировка по убыванию
-            PriceUp=false;
-            SortPriceUp.removeEventListener('click', function(){
-                let divRow = document.querySelector('div.row');
-                divRow.innerHTML ='';
-            });
-            
-
-            products.sort( 
+       // сорт по убыванию      
+        SortPriceDown.addEventListener('click', function(){
+            products.sort(
                 function(a, b) {
-                    return -(a.price - b.price);
+                     return -(a.price - b.price);
                 }
             );
-
-            SortPriceDown.addEventListener( 
-                        'click', function(){
-                            let divRow = document.querySelector('div.row');
-                            divRow.innerHTML = products.map(item => `
-                    
-                        <div class="cell p-2">
-                            <div class="card p-2 pt-4 h-100 shadow">
-                                <img src="${item.image}" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                <h5 class="card-title">${item.title}</h5>
-                                <p class="card-text">${item.description.substring(0, 100)}...</p>
-                                <h4 class='text-end'>$${item.price}</h4>
-                                </div>
-                            </div>
-                        </div>
-                    `).join('');
-                        }); 
-        }       
-
-        console.log(products);
-          
+            drawCards();
+            
+        });
+        
+        // сорт по возраст
+        SortPriceUp.addEventListener('click', function(){
+            products.sort(
+                function(a, b) {
+                     return a.price - b.price;
+                }
+            );
+            drawCards();    
+            
+        });          
+            
+            
+           
 
 
 
         //конец моего кода
         
-    
 
-    /*divRow.innerHTML = products.map(item => `
-    
-        <div class="cell p-2">
-            <div class="card p-2 pt-4 h-100 shadow">
-                <img src="${item.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${item.title}</h5>
-                <p class="card-text">${item.description.substring(0, 100)}...</p>
-                <h4 class='text-end'>$${item.price}</h4>
-                </div>
-            </div>
-        </div>
-    `).join('');*/
 
 
 })();
